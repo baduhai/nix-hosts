c2ba81896c9afc25fb424974420f52057406ac7e
William <williamhai@hotmail.com>
2022-07-25 10:41:56 +0200

    Fix indentation

--- sd-img-builder/sd-img.nix
+++ sd-img-builder/sd-img.nix
@@ -3,9 +3,8 @@
   imports = [
     <nixpkgs/nixos/modules/installer/sd-card/sd-image-aarch64.nix>
   ];
-  # put your own configuration here, for example ssh keys:
   users.extraUsers.root.openssh.authorizedKeys.keys = [
-     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA1v3+q3EaruiiStWjubEJWvtejam/r41uoOpCdwJtLL foxtrot@rotterdam"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA1v3+q3EaruiiStWjubEJWvtejam/r41uoOpCdwJtLL foxtrot@rotterdam"
   ];
   system.stateVersion = "22.05";
 }
