4e9606db56a246eed115d4b477cd9e0c8e1c4ab9
William <williamhai@hotmail.com>
2022-07-31 23:51:19 +0200

    I give up on openrgb

--- sd-img-builder/readme.md
+++ sd-img-builder/readme.md
@@ -3,3 +3,4 @@ To build the image, run
 `nix-build '<nixpkgs/nixos>' -A config.system.build.sdImage -I nixos-config=./sd-img.nix`  
 Note that this will build the image using the same channel as is currently in use by your system. To build it against a different channel, use:  
 `nix-build -I https://github.com/NixOS/nixpkgs/archive/refs/tags/22.05.tar.gz '<nixpkgs/nixos>' -A config.system.build.sdImage -I nixos-config=./sd-img.nix`
+I have had better luck building the image inside a docker container that is the same version as the desired image version.
--- sd-img-builder/sd-img.nix
+++ sd-img-builder/sd-img.nix
@@ -1,9 +1,14 @@
-{ ... }: {
+{ pkgs, ... }: {
   nixpkgs.crossSystem.system = "aarch64-linux";
   imports = [
     <nixpkgs/nixos/modules/installer/sd-card/sd-image-aarch64.nix>
   ];
-  users.extraUsers.root.openssh.authorizedKeys.keys = [
+  boot.kernelPackages = pkgs.linuxPackages_latest; # Raspberry pies have a hard time booting on the LTS kernel.
+  services.openssh = {
+    enable = true;
+    permitRootLogin = "yes";
+  };
+  users.users.root.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA1v3+q3EaruiiStWjubEJWvtejam/r41uoOpCdwJtLL foxtrot@rotterdam"
   ];
   system.stateVersion = "22.05";
